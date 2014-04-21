3832c2f5ee393fbb435e32168d51f21ddd343d2d
groob <vrancean@gmail.com>
2014-04-20 20:01:29 -0400

    workaround for an issue that wouldn't let me add fixtures. see http://stackoverflow.com/questions/22236029/trouble-getting-django-set-up-on-heroku-using-south-keep-getting-programminger

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 cd /home/app/sal && \
    sed -i "s/db_host/$DB_PORT_5432_TCP_ADDR/" sal/settings.py && \
-   python manage.py syncdb --noinput && \
-   python manage.py migrate && \
+   python manage.py syncdb --all --noinput && \
+   python manage.py migrate --fake && \
    python manage.py collectstatic --noinput
